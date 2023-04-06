6c1efc29d6559769d88a06f1e10bf9bbf6158bb0
unknown <leticie.outhierova@gmail.com>
2023-04-06 11:01:13 +0200

    theme colors defined

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,19 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 import { TeamsButtons } from "./TeamsButtons";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Typography } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#02D076'
+    },
+    secondary: {
+      main: '#10BFFC'
+    }
+  }
+})
 
 function App() {
   const [teams, setTeams] = useState();
@@ -39,12 +51,12 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <Typography variant="h1" component="h2">
+    <ThemeProvider theme={theme}>
+      <Typography variant="h1" component="h1">
         Teams
       </Typography>
       <TeamsButtons />
-    </div>
+    </ThemeProvider>
   );
 }
 
--- src/TeamsButtons.tsx
+++ src/TeamsButtons.tsx
@@ -1,6 +1,10 @@
+import { Button, ButtonGroup } from "@mui/material"
+
 export const TeamsButtons = () => {
     return (
-        <>
-        </>
+        <ButtonGroup variant="contained">
+            <Button color="primary">raff</Button>
+            <Button color="secondary">raff</Button>
+        </ButtonGroup>
     )
 }
