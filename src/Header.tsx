7d79c1dca93149b7a77ff5ede98b80bd7f4de54d
unknown <leticie.outhierova@gmail.com>
2023-04-07 11:49:37 +0200

    added header grid

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,29 +1,31 @@
-import { AppBar, Grid, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Grid, Toolbar, Typography } from "@mui/material";
 import { ReactComponent as Logo } from "./assets/alveno-logo.svg";
 import GroupsIcon from "@mui/icons-material/Groups";
 
 export const Header = () => {
   return (
     <>
-      <AppBar position="static" color="secondary">
+      <AppBar position="static">
         <Toolbar>
+
           <Grid
             container
-            columnSpacing={3}
+            spacing={3}
             justifyContent="center"
           >
-            <Grid item>
+            <Grid item xs={3}>
               <Logo />
             </Grid>
-            <Grid item>
+            <Grid item xs={6} >
               <Typography variant="h1" component="h1">
                 Teams
               </Typography>
             </Grid>
-            <Grid item>
+            <Grid item xs={3}>
               <GroupsIcon color="primary" ></GroupsIcon>
             </Grid>
           </Grid>
+
         </Toolbar>
       </AppBar>
     </>
