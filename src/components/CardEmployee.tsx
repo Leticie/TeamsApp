a3cd90c1393ad2e84c04448117e386e4cc48f81b
unknown <leticie.outhierova@gmail.com>
2023-04-09 18:14:07 +0200

    CardEmployee sx added to styles

--- src/components/CardEmployee.tsx
+++ src/components/CardEmployee.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent, Typography } from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
+import { CardEmployeeActiveS, CardEmployeePassiveS } from "../styles/CardEmployee.styles";
 
 interface CardEmployeeI {
     employee: {
@@ -19,7 +20,7 @@ export const CardEmployee = ({ employee }: CardEmployeeI) => {
     if (employee.endDate) {
       return (
         <Card
-          sx={{ padding: 4, bgcolor: "grey", width: "200px", heigth: "200px" }}
+          sx={CardEmployeePassiveS}
         >
           <CardEmployeeContent employee={employee}></CardEmployeeContent>
         </Card>
@@ -27,12 +28,7 @@ export const CardEmployee = ({ employee }: CardEmployeeI) => {
     }
     return (
       <Card
-        sx={{
-          padding: 4,
-          bgcolor: "primary.dark",
-          minWidth: "200px",
-          minHeigth: "200px",
-        }}
+        sx={CardEmployeeActiveS}
       >
         <CardEmployeeContent employee={employee}></CardEmployeeContent>
       </Card>
@@ -44,13 +40,11 @@ export const CardEmployee = ({ employee }: CardEmployeeI) => {
       <PersonIcon sx={{ fontSize: "11rem", justifyContent: "center" }} />
       <Typography
         variant="h5"
-        component="div"
         textAlign="center"
         fontSize="20px"
       >{`${employee.name} ${employee.surname}`}</Typography>
       <Typography
         variant="overline"
-        component="div"
         textAlign="center"
       >{`${employee.position}`}</Typography>
     </CardContent>
